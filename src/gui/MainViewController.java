79b54e8a8297bffe17437f18671011626b1e723d
Matheus Santos <matheus.san99@gmail.com>
2019-09-11 15:44:23 -0300

    botões 0-9 implementados / botão +

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -13,6 +13,10 @@ public class MainViewController {
     private String text;
 
     private boolean operationPressed;
+    private boolean plusPressed;
+    private boolean minusPressed;
+    private boolean multPressed;
+    private boolean divPressed;
 
     @FXML
     private TextField textField;
@@ -58,8 +62,31 @@ public class MainViewController {
     private Button btErase;
 
     public void onBt1Action(ActionEvent event) {
-        if(textField == null){
+        if(textField.getText() == null){
             textField.setText("1");
+
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(textField.getText());
+            } else {
+                number1 = Integer.parseInt(textField.getText());
+            }
+        } else {
+            text = textField.getText();
+            textField.setText(text + "1");
+
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(textField.getText());
+            } else {
+                number1 = Integer.parseInt(textField.getText());
+            }
+        }
+    }
+
+    public void onBt2Action(ActionEvent event) {
+        if(textField.getText() == null){
+            textField.setText("2");
 
             text = textField.getText();
             if (operationPressed == true) {
@@ -69,43 +96,226 @@ public class MainViewController {
             }
         } else {
             text = textField.getText();
-            textField.setText(text + "1");
+            textField.setText(text + "2");
+
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(text);
+            } else {
+                number1 = Integer.parseInt(text);
+            }
         }
     }
 
-    public void onBt2Action(ActionEvent event) {
-
-    }
     public void onBt3Action(ActionEvent event) {
+        if(textField.getText() == null){
+            textField.setText("3");
 
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(text);
+            } else {
+                number1 = Integer.parseInt(text);
+            }
+        } else {
+            text = textField.getText();
+            textField.setText(text + "3");
+
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(text);
+            } else {
+                number1 = Integer.parseInt(text);
+            }
+        }
     }
+
     public void onBt4Action(ActionEvent event) {
+        if(textField.getText() == null){
+            textField.setText("4");
 
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(text);
+            } else {
+                number1 = Integer.parseInt(text);
+            }
+        } else {
+            text = textField.getText();
+            textField.setText(text + "4");
+
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(text);
+            } else {
+                number1 = Integer.parseInt(text);
+            }
+        }
     }
+
     public void onBt5Action(ActionEvent event) {
+        if(textField.getText() == null){
+            textField.setText("5");
 
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(text);
+            } else {
+                number1 = Integer.parseInt(text);
+            }
+        } else {
+            text = textField.getText();
+            textField.setText(text + "5");
+
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(text);
+            } else {
+                number1 = Integer.parseInt(text);
+            }
+        }
     }
+
     public void onBt6Action(ActionEvent event) {
+        if(textField.getText() == null){
+            textField.setText("6");
+
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(text);
+            } else {
+                number1 = Integer.parseInt(text);
+            }
+        } else {
+            text = textField.getText();
+            textField.setText(text + "6");
+
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(text);
+            } else {
+                number1 = Integer.parseInt(text);
+            }
+        }
 
     }
+
     public void onBt7Action(ActionEvent event) {
+        if(textField.getText() == null){
+            textField.setText("7");
+
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(text);
+            } else {
+                number1 = Integer.parseInt(text);
+            }
+        } else {
+            text = textField.getText();
+            textField.setText(text + "7");
+
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(text);
+            } else {
+                number1 = Integer.parseInt(text);
+            }
+        }
 
     }
+
     public void onBt8Action(ActionEvent event) {
+        if(textField.getText() == null){
+            textField.setText("8");
+
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(text);
+            } else {
+                number1 = Integer.parseInt(text);
+            }
+        } else {
+            text = textField.getText();
+            textField.setText(text + "8");
+
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(text);
+            } else {
+                number1 = Integer.parseInt(text);
+            }
+        }
 
     }
+
     public void onBt9Action(ActionEvent event) {
+        if(textField.getText() == null){
+            textField.setText("9");
+
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(text);
+            } else {
+                number1 = Integer.parseInt(text);
+            }
+        } else {
+            text = textField.getText();
+            textField.setText(text + "9");
+
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(text);
+            } else {
+                number1 = Integer.parseInt(text);
+            }
+        }
 
     }
+
     public void onBt0Action(ActionEvent event) {
+        if(textField.getText() == null){
+            textField.setText("0");
+
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(text);
+            } else {
+                number1 = Integer.parseInt(text);
+            }
+        } else {
+            text = textField.getText();
+            textField.setText(text + "0");
+
+            text = textField.getText();
+            if (operationPressed == true) {
+                number2 = Integer.parseInt(text);
+            } else {
+                number1 = Integer.parseInt(text);
+            }
+        }
 
     }
-//    public void onBtAction(ActionEvent event) {
+
+    public void onBtPlusAction(ActionEvent event) {
+        if (textField.getText() == null || textField.getText() == ""){
+            number1 = 0;
+        }
+        if (plusPressed || minusPressed || multPressed || divPressed){
+            plusPressed = true;
+            minusPressed = false;
+            multPressed = false;
+            divPressed = false;
+            textField.setText(null);
+        } else {
+            operationPressed = true;
+            textField.setText(null);
+        }
+    }
+
+//    public void onBtEqualAction(ActionEvent event) {
 //
 //    }
-//    public void onBt1Action(ActionEvent event) {
-//
-//    }
+
 //    public void onBt1Action(ActionEvent event) {
 //
 //    }
